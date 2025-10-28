7574d3a6c4ab9540b904dccf695ebad2004d7ca4
Arta Shah <77407261+ArtaShah@users.noreply.github.com>
2025-10-29 01:12:10 +0330

    Add files via upload

---
+++ Roman_Intiger.py
@@ -0,0 +1,19 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman = {
+            'I': 1, 'V': 5, 'X': 10,
+            'L': 50, 'C': 100,
+            'D': 500, 'M': 1000
+        }
+        result = 0
+        prev_value = 0
+
+        for ch in reversed(s):
+            value = roman[ch]
+            if value < prev_value:
+                result -= value
+            else:
+                result += value
+            prev_value = value
+
+        return result
